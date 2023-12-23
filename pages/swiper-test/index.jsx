262b04c1bf83926f3b1da870d778ca3515d2ae02
amin <94073967+aminyaghmaee@users.noreply.github.com>
2023-12-23 12:09:48 +0330

    swiper test added

--- pages/swiper-test/index.jsx
+++ pages/swiper-test/index.jsx
@@ -7,46 +7,54 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import { useState } from "react";
 import TreeOne from "../../assets/images/mainpage/asd.jpg";
-import TreeTwo from "../../assets/images/mainpage/Untitled.jpg";
+import TreeTwo from "../../assets/images/mainpage/hero_main.png";
 
 function SwiperTest() {
   const [my_swiper, set_my_swiper] = useState({});
   return (
-    <div className="relative w-[70%] mx-auto">
+    <div className="relative w-[60%] mx-auto">
       <div className="absolute top-[160px] w-full z-10">
-        <div className="w-20 h-20 rounded-full bg-white flex justify-center items-center absolute right-[4%]">
-          <svg
-            onClick={() => my_swiper.slideNext()}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6  cursor-pointer"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m8.25 4.5 7.5 7.5-7.5 7.5"
-            />
-          </svg>
+        <div className="w-10 h-10 clippath1 absolute bg-white top-[-27px] right-[-1px]"></div>
+        <div className="w-10 h-10 clippath1 absolute bg-white swiper-rotate1 top-[69px] right-[-1px]"></div>
+        <div className="w-10 h-10 clippath1 absolute bg-white left-[-1px] swiper-rotate2 top-[-27px]"></div>
+        <div className="w-10 h-10 clippath1 absolute bg-white left-[-1px] rotate-180 top-[4.2rem]"></div>
+        <div className="w-20 h-20 rounded-full bg-white flex justify-center items-center absolute right-[-40px]">
+          <div className="w-12 h-12 border border-[#B27C00] rounded-full flex justify-center items-center">
+            <svg
+              onClick={() => my_swiper.slideNext()}
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 cursor-pointer text-[#4C3500]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </div>
         </div>
-        <div className="w-20 h-20 rounded-full bg-white flex justify-center items-center absolute left-[4%]">
-          <svg
-            onClick={() => my_swiper.slidePrev()}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6  cursor-pointer"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5 8.25 12l7.5-7.5"
-            />
-          </svg>
+        <div className="w-20 h-20 rounded-full bg-white flex justify-center items-center absolute left-[-40px]">
+          <div className="w-12 h-12 border border-[#B27C00] rounded-full flex justify-center items-center">
+            <svg
+              onClick={() => my_swiper.slidePrev()}
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 cursor-pointer text-[#4C3500]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
+            </svg>
+          </div>
         </div>
       </div>
       <Swiper
@@ -56,8 +64,9 @@ function SwiperTest() {
         style={{
           display: "flex",
           justifyContent: "center",
-          width: "90%",
+          width: "100%",
           zIndex: "0",
+          pointerEvents: "none",
         }}
         modules={[Scrollbar]}
         spaceBetween={50}
@@ -65,10 +74,10 @@ function SwiperTest() {
         autoplay={{ delay: 3000, disableOnInteraction: true }}
       >
         <SwiperSlide>
-          <img className="w-full h-[400px]" src={TreeOne} alt="a" />
+          <div className="w-full h-[400px] bg-gray-400"></div>
         </SwiperSlide>
         <SwiperSlide>
-          <img className="w-full h-[400px]" src={TreeTwo} alt="b" />
+          <div className="w-full h-[400px] bg-black"></div>
         </SwiperSlide>
       </Swiper>
     </div>
