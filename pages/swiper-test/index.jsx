59e91a9a17b2b3060278d0fb99e21b58b470de7e
amin <94073967+aminyaghmaee@users.noreply.github.com>
2023-12-23 12:50:08 +0330

    v

--- pages/swiper-test/index.jsx
+++ pages/swiper-test/index.jsx
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Scrollbar } from "swiper/modules";
 import "swiper/css";
 import "swiper/css";
 import "swiper/css/navigation";
