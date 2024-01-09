aad946de595f481b874e75b34f5dbb2b91060657
alitaghvayi7 <[alitaghvaei78@gmail.com]>
2024-01-09 12:03:28 +0330

    add map to add business

---
+++ components/shared/GoogleMapNoControll.tsx
@@ -0,0 +1,97 @@
+import {MapContainer, Marker, TileLayer, useMap} from "react-leaflet";
+import {useEffect, useLayoutEffect, useRef, useState} from "react";
+import {Icon, Map} from "leaflet";
+import IconImage from "@/assets/images/icons/icons8_Location_1.png";
+
+interface Props {
+    boundes: { lat: number, lng: number };
+    setBoundes: Function;
+}
+
+const GoogleMapNoControll = (props:Props) => {
+    const {boundes, setBoundes} = props;
+    const [loaded, setLoaded] = useState(false);
+    const mapRef = useRef<Map | null>(null);
+
+    const markerIcon = new Icon({
+        iconSize: [40, 40],
+        iconUrl: IconImage.src,
+    });
+
+    useLayoutEffect(() => {
+        if ("geolocation" in navigator) {
+            // Prompt user for permission to access their location
+            navigator.geolocation.getCurrentPosition(
+                // Success callback function
+                (position) => {
+                    // Get the user's latitude and longitude coordinates
+                    const lat = position.coords.latitude;
+                    const lng = position.coords.longitude;
+
+                    // Do something with the location data, e.g. display on a map
+                    // console.log(`Latitude: ${lat}, longitude: ${lng}`);
+                    setBoundes({ lat, lng });
+                },
+                // Error callback function
+                (error) => {
+                    // Handle errors, e.g. user denied location sharing permissions
+                    // console.error("Error getting user location:", error);
+                    setBoundes({ lat: 34.382436, lng: 50.523504 });
+                }
+            );
+        } else {
+            // Geolocation is not supported by the browser
+            // console.error("Geolocation is not supported by this browser.");
+            setBoundes({ lat: 34.382436, lng: 50.523504 });
+        }
+    }, []);
+
+    return(
+        <>
+            <section className={`w-full h-full overflow-hidden relative isolate`}>
+
+                <MapContainer
+                    ref={mapRef}
+                    className={`w-full h-full absolute inset-0 z-[1]`}
+                    center={[boundes?.lat, boundes?.lng]}
+                    zoom={30}
+                    scrollWheelZoom={false}
+                    fadeAnimation={true}
+                    zoomControl={false}
+                    style={{background: "transparent"}}
+                >
+                    <TileLayer
+                        url={`http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}`}
+                        subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                        attribution='&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+
+                    <Marker icon={markerIcon} position={boundes}></Marker>
+                    <RecenterAutomatically lat={boundes?.lat} lng={boundes?.lng}/>
+                </MapContainer>
+            </section>
+        </>
+    )
+}
+
+const RecenterAutomatically = ({lat, lng}: { lat: number; lng: number }) => {
+    const map = useMap();
+
+
+    useEffect(() => {
+        map.setView([lat, lng]);
+    }, [lat, lng, map]);
+
+    useEffect(() => {
+        map.off('click');
+        map.off('dblclick');
+        map.off('touchstart');
+        map.off('touchmove');
+        map.off('touchend');
+        map.dragging.disable();
+    }, []);
+
+    return null;
+};
+
+export default GoogleMapNoControll;
--- pages/introduction/index.tsx
+++ pages/introduction/index.tsx
@@ -1,18 +1,66 @@
 import Menu from "@/components/shared/Menu";
-import ProfileLayout from "@/pages/ProfileLayout";
-import {Fragment, useState} from "react";
-import {ArrowRightIcon, ChatBubbleLeftIcon, ChevronDownIcon, PhotoIcon} from "@heroicons/react/24/outline";
-import Link from "next/link";
-import Image from "next/image";
-import {StarIcon} from "@heroicons/react/24/solid";
+import {useEffect, useState} from "react";
+import {ChevronDownIcon, PhotoIcon, XMarkIcon} from "@heroicons/react/24/outline";
 import Footer from "@/components/shared/Footer";
+// import GoogleMapNoControll from "@/components/shared/GoogleMapNoControll";
+import dynamic from "next/dynamic";
+import {CloseIcon} from "next/dist/client/components/react-dev-overlay/internal/icons/CloseIcon";
+// import GoogleMapWithControll from "@/components/shared/GoogleMapWithControll";
+const GoogleMapNoControll = dynamic(import("@/components/shared/GoogleMapNoControll"), {ssr: false})
+const GoogleMapWithControll = dynamic(import("@/components/shared/GoogleMapWithControll"), {ssr: false})
 
 export default function IntroductionPage() {
     const [selected, setSelected] = useState<null | string>(null);
     const [open, setOpen] = useState(false);
+    const [showFirstMap, setShowFirstMap] = useState(false);
+    const [showSecondMap, setShowSecondMap] = useState(false);
+    const [userLocation, setUserLocation] = useState({lng: 0, lat: 0});
+    const [finalLocation, setFinalLocation] = useState({lng: 0, lat: 0});
+    const [address, setAddress] = useState<{address_line2:''} | null>(null);
+
+    useEffect(() => {
+        const requestOptions = {
+            method: 'GET',
+        }
+
+        fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${finalLocation?.lat}&lon=${finalLocation?.lng}&apiKey=${process.env.GEOAPIFY_API_KEY}`, requestOptions)
+            .then(response => response.json())
+            .then(result => setAddress(result.features[0].properties))
+            .catch(error => console.log('error', error));
+    }, [finalLocation]);
 
     return (
         <>
+            {showFirstMap && <div
+                className={`w-screen h-screen bg-[rgba(255,255,255,.1)] backdrop-blur-[4px] fixed inset-0 z-[100] flex items-center justify-center`}>
+                <XMarkIcon onClick={()=>setShowFirstMap(false)} className={`w-8 h-8 text-gray-04 absolute top-10 left-10 cursor-pointer`}/>
+                <div
+                    className={`w-[90%] lg:w-[40%] h-[50%] bg-white rounded-[32px] shadow-2xl flex flex-col justify-between px-6 py-4`}>
+                    <div className={`w-full h-[80%] rounded-[16px] overflow-hidden`}>
+                        <GoogleMapNoControll boundes={userLocation} setBoundes={setUserLocation}/>
+                    </div>
+                    <button
+                        onClick={() => {setFinalLocation(userLocation);setShowFirstMap(false)}}
+                        className={`w-full lg:w-[346px] mx-auto h-[40px] rounded-[8px] bg-yellow-main text-write-main font-[400] text-[16px] flex items-center justify-center`}>
+                        ثبت
+                    </button>
+                </div>
+            </div>}
+            {showSecondMap && <div
+                className={`w-screen h-screen bg-[rgba(255,255,255,.1)] backdrop-blur-[4px] fixed inset-0 z-[100] flex items-center justify-center`}>
+                <XMarkIcon onClick={()=>setShowSecondMap(false)} className={`w-8 h-8 text-gray-04 absolute top-10 left-10 cursor-pointer`}/>
+                <div
+                    className={`w-[90%] lg:w-[40%]  h-[60%] bg-white rounded-[32px] shadow-2xl flex flex-col justify-between px-6 py-4`}>
+                    <div className={`w-full h-[80%] rounded-[16px] overflow-hidden`}>
+                        <GoogleMapWithControll boundes={userLocation} setBoundes={setUserLocation}/>
+                    </div>
+                    <button
+                        onClick={() => {setFinalLocation(userLocation);setShowSecondMap(false)}}
+                        className={`w-full lg:w-[346px] mx-auto h-[40px] rounded-[8px] bg-yellow-main text-write-main font-[400] text-[16px] flex items-center justify-center`}>
+                        ثبت
+                    </button>
+                </div>
+            </div>}
             <main className={`w-full`}>
                 <header className={`w-full bg-blue-04 bg-opacity-20`}>
                     <Menu/>
@@ -59,8 +107,10 @@ export default function IntroductionPage() {
                             </ul>
                         </details>
                         <form className={`w-full mt-6 flex flex-col items-center gap-6`}>
-                            <div className={`w-full h-max lg:h-[470px] lg:py-10 lg:px-[64px] rounded-[32px] bg-transparent lg:bg-white lg:border lg:border-yellow-04 flex flex-wrap gap-4 lg:gap-0 items-center justify-between`}>
-                                <div className={`h-full w-full max-w-[539px] flex flex-col justify-between gap-4 lg:gap-0`}>
+                            <div
+                                className={`w-full h-max lg:h-[470px] lg:py-10 lg:px-[64px] rounded-[32px] bg-transparent lg:bg-white lg:border lg:border-yellow-04 flex flex-wrap gap-4 lg:gap-0 items-center justify-between`}>
+                                <div
+                                    className={`h-full w-full max-w-[539px] flex flex-col justify-between gap-4 lg:gap-0`}>
                                     <input
                                         type={'text'}
                                         placeholder={`نوع کسب و کار`}
@@ -98,12 +148,22 @@ export default function IntroductionPage() {
                                     <label
                                         className={`w-full h-[216px] block relative isolate border border-gray-02 rounded-[16px] overflow-hidden`}>
                                         <textarea
+                                            value={address?.address_line2 ?? ""}
                                             placeholder={'آدرس را وارد کنید'}
                                             className={`w-full h-full absolute inset-0 z-[1] bg-white text-write-main placeholder:text-gray-03 p-6 text-[16px] font-[400] resize-none`}
                                         ></textarea>
-                                        <div className={`w-full h-[56px] flex items-center justify-between absolute z-[2] px-6 left-0 right-0 bottom-4`}>
-                                            <button type={'button'} className={`w-full max-w-[225px] border border-yellow-04 text-[14px] text-gray-03 rounded-[8px] py-[10px] flex items-center justify-center cursor-pointer`}>من اینجام</button>
-                                            <button type={'button'} className={`w-full max-w-[225px] border border-yellow-04 text-[14px] text-gray-03 rounded-[8px] py-[10px] flex items-center justify-center cursor-pointer`}>انتخاب از روی نقشه</button>
+                                        <div
+                                            className={`w-full h-[56px] flex items-center justify-between absolute z-[2] px-6 left-0 right-0 bottom-4`}>
+                                            <button type={'button'}
+                                                    onClick={() => setShowFirstMap(true)}
+                                                    className={`w-full max-w-[225px] border border-yellow-04 text-[14px] text-gray-03 rounded-[8px] py-[10px] flex items-center justify-center cursor-pointer`}>من
+                                                اینجام
+                                            </button>
+                                            <button type={'button'}
+                                                    onClick={() => setShowSecondMap(true)}
+                                                    className={`w-full max-w-[225px] border border-yellow-04 text-[14px] text-gray-03 rounded-[8px] py-[10px] flex items-center justify-center cursor-pointer`}>انتخاب
+                                                از روی نقشه
+                                            </button>
                                         </div>
                                     </label>
 
@@ -124,3 +184,5 @@ export default function IntroductionPage() {
         </>
     )
 }
+
+
