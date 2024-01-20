35239cfae883e574c88124dc5c21e5e24bf8d16e
alitaghvayi7 <alitaghvaei78@gmil.com>
2024-01-20 12:49:32 +0330

    update enviroments

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   env: {
     THUNDERFOREST_API_KEY: process.env.THUNDERFOREST_API_KEY,
     GEOAPIFY_API_KEY:process.env.GEOAPIFY_API_KEY,
+    BACKEND_URL:process.env.BACKEND_URL,
+    BACKEND_VERSION:process.env.BACKEND_VERSION
   },
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,18 @@ export default function App({
   const { store } = wrapper.useWrappedStore(rest);
   return (
     <>
-      <NextTopLoader color={"#FED474"} />
+      <NextTopLoader color={"#FED474"} initialPosition={0.08}
+        crawlSpeed={200}
+        height={3}
+        crawl={true}
+        showSpinner={true}
+        easing="ease"
+        speed={200}
+        shadow="0 0 10px #4C82FE,0 0 5px #4C82FE"
+        template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+        zIndex={1600}
+        showAtBottom={false} />
       <SessionProvider session={pageProps.session}>
         <Provider store={store}>
           <PersistGate persistor={store.persistor}>
