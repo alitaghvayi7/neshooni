66c75d7f4f691ad41ae8a893e98bcab8d71cce7b
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2024-02-29 14:21:14 +0330

    edit ui

--- features/HomePage/ShoppingSection.tsx
+++ features/HomePage/ShoppingSection.tsx
@@ -47,7 +47,9 @@ const slides = [
 const ShoppingSection = () => {
   return (
     <>
-      <section className={`flex flex-col items-center  w-[calc(100%-56px)] lg:w-[calc(100%-128px)] max-w-[1689px] mx-auto mt-[11rem] lg:mt-[15rem] gap-8`}>
+      <section
+        className={`flex flex-col items-center  w-[calc(100%-56px)] lg:w-[calc(100%-128px)] max-w-[1689px] mx-auto mt-[11rem] lg:mt-[15rem] gap-8`}
+      >
         <div className={`w-full flex items-center justify-between`}>
           <span
             style={{ backgroundClip: "content-box", borderRadius: "0 0 32px 0" }}
@@ -90,23 +92,88 @@ const ShoppingSection = () => {
               allowTouchMove={true}
               breakpoints={{
                 // when window width is >= 320px
-                320: {
-                  slidesPerView: 1,
-                  spaceBetween: 20,
-                },
+                // 320: {
+                //   slidesPerView: 1,
+                //   spaceBetween: 20,
+                // },
                 // when window width is >= 480px
-                480: {
+                460: {
+                  slidesPerView: 1.7,
+                  spaceBetween: 25,
+                },
+                // when window width is >= 520px
+                520: {
                   slidesPerView: 2,
                   spaceBetween: 25,
                 },
+                // when window width is >= 580px
+                580: {
+                  slidesPerView: 2.4,
+                  spaceBetween: 30,
+                },
                 // when window width is >= 640px
                 640: {
-                  slidesPerView: 3,
+                  slidesPerView: 2.7,
                   spaceBetween: 25,
                 },
+                // when window width is >= 730px
+                730: {
+                  slidesPerView: 3.2,
+                  spaceBetween: 25,
+                },
+                // when window width is >= 820px
+                820: {
+                  slidesPerView: 3.6,
+                  spaceBetween: 25,
+                },
+                // when window width is >= 900px
+                900: {
+                  slidesPerView: 4.1,
+                  spaceBetween: 25,
+                },
+                // when window width is >= 1024px
+                1024: {
+                  slidesPerView: 1.2,
+                  spaceBetween: 25,
+                },
+                // when window width is >= 1100px
+                1100: {
+                  slidesPerView: 1.4,
+                  spaceBetween: 16,
+                },
+                // when window width is >= 1200px
+                1200: {
+                  slidesPerView: 1.6,
+                  spaceBetween: 16,
+                },
+                // when window width is >= 1300px
+                1300: {
+                  slidesPerView: 1.9,
+                  spaceBetween: 16,
+                },
+                // when window width is >= 1400px
+                1400: {
+                  slidesPerView: 2.2,
+                  spaceBetween: 16,
+                },
+                // when window width is >= 1500px
+                1500: {
+                  slidesPerView: 2.5,
+                  spaceBetween: 16,
+                },
+                // when window width is >= 1620px
+                1620: {
+                  slidesPerView: 2.8,
+                  spaceBetween: 16,
+                },
+                // when window width is >= 1720px
+                1720: {
+                  slidesPerView: 3.1,
+                  spaceBetween: 16,
+                },
               }}
-              spaceBetween={25}
-              slidesPerView={3}
+              spaceBetween={20}
+              slidesPerView={1.2}
               pagination={{ clickable: true, enabled: false }}
               scrollbar={{ draggable: true }}
               style={{
@@ -114,9 +181,15 @@ const ShoppingSection = () => {
                 height: "100%",
               }}
             >
-              {slides.map((item: any) => {
+              {slides.map((item: any, index: number) => {
                 return (
-                  <SwiperSlide key={item?.id}>
+                  <SwiperSlide
+                    key={item?.id}
+                    className={``}
+                    // style={{
+                    //   zIndex: index + 5,
+                    // }}
+                  >
                     <div className="w-[169px] lg:w-[350px] h-full rounded-[16px] overflow-hidden flex flex-col ">
                       <Image alt={``} src={item?.image} className={`w-full h-[66%] object-cover`} />
                       <div
